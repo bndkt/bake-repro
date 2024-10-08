584f2ce136b63ad08bfe922cf1ee418556630082
Benedikt Müller <91166910+bndkt@users.noreply.github.com>
2024-10-08 08:45:15 +0200

    Fix imports

--- apps/web/manual.ts
+++ apps/web/manual.ts
@@ -1,11 +1,15 @@
 import { wipDevServerExpectHugeBreakingChanges } from "bun";
 
+// Test imports
+// import { onServerSideReload } from "bun-framework-rsc/client";
+// import server from "bun-framework-rsc/server";
+
 import { routes } from "./routes";
 
 wipDevServerExpectHugeBreakingChanges({
 	framework: {
-		serverEntryPoint: "./bun-framework-src/server.tsx",
-		clientEntryPoint: "./bun-framework-src/client.tsx",
+		serverEntryPoint: "bun-framework-src/server",
+		clientEntryPoint: "bun-framework-src/client",
 		builtInModules: {},
 		serverComponents: {},
 		separateSSRGraph: true,
